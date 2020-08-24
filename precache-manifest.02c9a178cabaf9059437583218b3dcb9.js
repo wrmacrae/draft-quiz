991e518aa3bef31e888f7f53615209dc95d34186
Will Macrae <wmacrae@palantir.com>
2020-08-23 19:34:12 -0700

    Updates

---
+++ precache-manifest.02c9a178cabaf9059437583218b3dcb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2edaf3c1a9e9336711c02ea0172da5",
+    "url": "/draft-quiz/index.html"
+  },
+  {
+    "revision": "6f5cb1925774779de1cc",
+    "url": "/draft-quiz/static/css/main.c2b91a24.chunk.css"
+  },
+  {
+    "revision": "8bc7ea402a142cc038f0",
+    "url": "/draft-quiz/static/js/2.be004951.chunk.js"
+  },
+  {
+    "revision": "be14b386ccc34a49dee76740150eacbc",
+    "url": "/draft-quiz/static/js/2.be004951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f5cb1925774779de1cc",
+    "url": "/draft-quiz/static/js/main.c0897aa5.chunk.js"
+  },
+  {
+    "revision": "804e571c0171350041e3",
+    "url": "/draft-quiz/static/js/runtime-main.92cf1982.js"
+  },
+  {
+    "revision": "91e66f754650d98be0cc3e95d8c46484",
+    "url": "/draft-quiz/static/media/master.91e66f75.zip"
+  }
+]);
