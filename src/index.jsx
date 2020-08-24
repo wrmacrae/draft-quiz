bf7852a636f751500a9a8d1a4e710c1dd52643af
Will Macrae <wmacrae@palantir.com>
2020-08-24 09:37:20 -0700

    accept id param

--- src/index.jsx
+++ src/index.jsx
@@ -7,21 +7,25 @@ import { setDraft } from './actions'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const ids = [
-  "3d2a03b48a1a431bacb591b05af66b1f",
-  "2c7465b07b44468684107bc19ea3f81b",
-  "de68d9cb91894f748e4b29c990a759b9",
-  "a0483a48a14b4142b35cd1f0c4523652",
-  "29fa2afbd9c94bee9c550d79360f35d1",
-  "43cb9b90c3a24858952cc879a45fa0a4",
-  "497ad517ae2a402e92c4114b281f60c8",
-  "5d51884623264169b194763bd9c6f5b7",
-  "de5fcd6ad83d4444a780db8b4b1fe42d",
-  "80166d4735ec407496256b13ddeaa89d",
-];
-const id = ids[Math.floor(Math.random() * ids.length)];
+function getId() {
+  let idFromParam = (new URLSearchParams(window.location.search)).get('id');
+  const ids = [
+    "3d2a03b48a1a431bacb591b05af66b1f",
+    "2c7465b07b44468684107bc19ea3f81b",
+    "de68d9cb91894f748e4b29c990a759b9",
+    "a0483a48a14b4142b35cd1f0c4523652",
+    "29fa2afbd9c94bee9c550d79360f35d1",
+    "43cb9b90c3a24858952cc879a45fa0a4",
+    "497ad517ae2a402e92c4114b281f60c8",
+    "5d51884623264169b194763bd9c6f5b7",
+    "de5fcd6ad83d4444a780db8b4b1fe42d",
+    "80166d4735ec407496256b13ddeaa89d",
+  ];
+  return idFromParam || ids[Math.floor(Math.random() * ids.length)];  
+}
 
-const store = createStore(reducer, reducer({}, setDraft(id)));
+
+const store = createStore(reducer, reducer({}, setDraft(getId())));
 
 render(
   <Provider store={store}>
