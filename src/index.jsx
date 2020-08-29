0313888ea5e02ef65abd17271cbef4d1781f5f1e
Will Macrae <wmacrae@palantir.com>
2020-08-28 20:58:05 -0700

    Different sets, 6 M21 logs

--- src/index.jsx
+++ src/index.jsx
@@ -6,16 +6,19 @@ import reducer from './reducer'
 import { setDraft } from './actions'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
+import _ from 'lodash';
 
 function getId() {
-  let idFromParam = (new URLSearchParams(window.location.search)).get('id');
+  const idFromParam = (new URLSearchParams(window.location.search)).get('id');
   if (idFromParam) {
     return idFromParam
   }
-  const logs = require('./logs');
+  const setFromParam = (new URLSearchParams(window.location.search)).get('set');
+  const set = setFromParam || "AKR";
+  var logs = require('./logs');
+  logs = _.pickBy(logs, log => log.set === set);
   const ids = Object.keys(logs);
-  return idFromParam || ids[Math.floor(Math.random() * ids.length)];  
+  return ids[Math.floor(Math.random() * ids.length)];  
 }
 
 
