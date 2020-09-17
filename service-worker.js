8d6dfb97dcbb86680650e7f8542c845d0ff93fc0
Will Macrae <wmacrae@palantir.com>
2020-09-16 19:12:48 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/draft-quiz/precache-manifest.9bf4a35fb4ca75ab84b747b1e320ba1b.js"
+  "/draft-quiz/precache-manifest.51951c71421a27af280ecd99a8573c7d.js"
 );
 
 self.addEventListener('message', (event) => {
