63255c2db4a1ea5790bc9cd492ac6b5f8920f4cf
Will Macrae <wmacrae@palantir.com>
2020-09-24 22:37:20 -0700

    add a draft, improve add-draft.sh

--- src/add-draft.sh
+++ src/add-draft.sh
@@ -1,1 +1,3 @@
+set="ZNR"
+record="7-0"
 curl -sS https://www.17lands.com/data/draft?draft_id=$id > $id.json && jq -s ".[0] * {\"$id\":(.[1] * {\"set\": \"$set\", \"source\": \"17lands.com/trophies\", \"record\": \"$record\", \"type\": \"Premier\"})}" logs.json $id.json > newlogs.json && mv newlogs.json logs.json && rm $id.json
