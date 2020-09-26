8c8f596a77a8e582796c7fcace9fa5d2f258a00a
Will Macrae <wmacrae@palantir.com>
2020-09-25 21:12:58 -0700

    znr traditional log

--- src/add-draft.sh
+++ src/add-draft.sh
@@ -1,3 +1,4 @@
 set="ZNR"
 record="7-0"
-curl -sS https://www.17lands.com/data/draft?draft_id=$id > $id.json && jq -s ".[0] * {\"$id\":(.[1] * {\"set\": \"$set\", \"source\": \"17lands.com/trophies\", \"record\": \"$record\", \"type\": \"Premier\"})}" logs.json $id.json > newlogs.json && mv newlogs.json logs.json && rm $id.json
+type="Premier"
+curl -sS https://www.17lands.com/data/draft?draft_id=$id > $id.json && jq -s ".[0] * {\"$id\":(.[1] * {\"set\": \"$set\", \"source\": \"17lands.com/trophies\", \"record\": \"$record\", \"type\": \"$type\"})}" logs.json $id.json > newlogs.json && mv newlogs.json logs.json && rm $id.json
